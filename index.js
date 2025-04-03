50e1b2c756f2b7e63deef6481a4d9713c42dd1e7
Karim Abdelkareem <alkar33m@gmail.com>
2025-04-03 17:35:49 +0200

    fixes2

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    origin: [
+      process.env.CLIENT_URL || "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
+    credentials: true,
     methods: ["GET", "POST"],
   },
 });
